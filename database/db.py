afbac5b9ab5eedce363d9c80b56019a70b334e6d
renathy <renaate@gmail.com>
2026-03-31 22:47:29 +0300

    Update db.py

--- database/db.py
+++ database/db.py
@@ -1,1 +1,9 @@
+import psycopg2
 
+def get_connection():
+    return psycopg2.connect(
+        host="localhost",
+        database="your_db",
+        user="your_user",
+        password="your_password"
+    )
